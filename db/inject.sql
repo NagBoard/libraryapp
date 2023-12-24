c05cfdf432f49d49f247067fdcc18021a3ac8368
NagBoard <312dasd123sdas12312@gmail.com>
2023-12-24 03:25:22 +0200

    populated main.db with sample data

---
+++ db/inject.sql
@@ -0,0 +1,11 @@
+INSERT INTO "book" (title, author, description, book_type, available_copies, total_copies) VALUES
+('A Game of Thrones', 'George R. R. Martin', 'The first book in the Song of Ice and Fire series.', 'Fantasy', 5, 5),
+('A Clash of Kings', 'George R. R. Martin', 'The second book in the Song of Ice and Fire series.', 'Fantasy', 5, 5),
+('A Storm of Swords', 'George R. R. Martin', 'The third book in the Song of Ice and Fire series.', 'Fantasy', 5, 5),
+('A Feast for Crows', 'George R. R. Martin', 'The fourth book in the Song of Ice and Fire series.', 'Fantasy', 5, 5),
+('A Dance with Dragons', 'George R. R. Martin', 'The fifth book in the Song of Ice and Fire series.', 'Fantasy', 5, 5),
+('Star Wars: Heir to the Empire', 'Timothy Zahn', 'The first book in the Star Wars: Thrawn trilogy.', 'Science Fiction', 5, 5),
+('Star Wars: Dark Force Rising', 'Timothy Zahn', 'The second book in the Star Wars: Thrawn trilogy.', 'Science Fiction', 5, 5),
+('Star Wars: The Last Command', 'Timothy Zahn', 'The third book in the Star Wars: Thrawn trilogy.', 'Science Fiction', 5, 5),
+('Star Wars: The Rising Force', 'Dave Wolverton', 'The first book in the Star Wars: Jedi Apprentice series.', 'Science Fiction', 5, 5),
+('Star Wars: The Dark Rival', 'Jude Watson', 'The second book in the Star Wars: Jedi Apprentice series.', 'Science Fiction', 5, 5);
